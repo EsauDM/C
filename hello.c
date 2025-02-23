4976b8c561e5c769640db4c71b6450cd8eb294f6
EsauDM <esau.ccbf@gmail.com>
2025-02-23 17:04:44 -0300

    user input

--- hello.c
+++ hello.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
-void main(){
-  printf("Hello, World!");
+
+int main(){ // user input and print output.
+  char answer[60];   
+  printf("What's your name? \n");
+  fgets(answer,sizeof(answer),stdin);
+  printf("Hello, %s \n", answer);
 }
