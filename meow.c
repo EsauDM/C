22cb32ebc0abf4ced31202590c4a9314adf05601
EsauDM <esau.ccbf@gmail.com>
2025-02-23 18:14:06 -0300

    loops

---
+++ meow.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*int main(void){
+  int i = 0;
+  while(i < 3){
+    printf("Meow \n");
+    i++;
+  }
+}*/
+
+int main(void){
+  for (int i = 0; i < 3; i++){
+    printf("meow\n");
+  }
+}
+
